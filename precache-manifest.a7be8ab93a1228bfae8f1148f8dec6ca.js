0b13a3654cd833c5d33ffe963ef88ee397144c99
tuilathang97 <50223061+tuilathang97@users.noreply.github.com>
2019-09-12 10:58:51 +0700

    Add files via upload

---
+++ precache-manifest.a7be8ab93a1228bfae8f1148f8dec6ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e30cbeef5a18bda02a282b5460e6bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bcbe40f3bc0f7f30792",
+    "url": "/static/css/main.322a1213.chunk.css"
+  },
+  {
+    "revision": "654f45d210f7a6d4823c",
+    "url": "/static/js/2.063319b9.chunk.js"
+  },
+  {
+    "revision": "1bcbe40f3bc0f7f30792",
+    "url": "/static/js/main.84818891.chunk.js"
+  },
+  {
+    "revision": "9628537df4edc88ca364",
+    "url": "/static/js/runtime~main.aa9b3280.js"
+  }
+]);
